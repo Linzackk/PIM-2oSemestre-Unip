2a503b4d9acb61b40b45d6898ba557a6de56314b
Isaac de Medeiros <isaac2005medeiros@gmail.com>
2025-09-15 18:57:12 -0300

    Organização de Pastas e Arquivos Criação das pastas "core" e "models" Core: Abriga os arquivos com funções relacionadas a: bancoDeDados, estrutura na saída do console e verificacao de entradas. Models: Abriga as classes "Aluno", "Professor" "Coordenacao" em seus respectivos arquivos

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+# Sistema Colaborativo de uma Plataforma de Educação Digital com apoio de Inteligência Artificial
+# TODO: Classes: Conta, Aluno, Professor, Coordenação
+# TODO: Banco de Dados, Tabelas: Login, Alunos, Professores, Coordenação, Campus, Cursos, Matérias e Notas
+
 print("Hello World!")
