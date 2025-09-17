9552590a8c13da22cdc9559aac0e5e34f0fe24b6
Isaac de Medeiros <isaac2005medeiros@gmail.com>
2025-09-17 15:18:25 -0300

    Testes com as funções de adição e leitura das informações no banco de dados

--- core/database/bancoDeDados.py
+++ core/database/bancoDeDados.py
@@ -10,7 +10,7 @@
 
 # professor = ["id_professor", "nome", "idade", "genero", "data_nascimento", "cpf", "materia_ensino"]
 
-# coordenacao = ["id_admin", "nome", "idade", "campus", "data_nascimento", "cpf", "cargo"]
+# coordenacao = ["id_admin", "nome", "idade", "genero" "campus", "data_nascimento", "cpf", "cargo"]
 
 # campus = ["id_campus", "endereco", "diretor", "diretor_id", "telefone"]  # Corrigi a vírgula que estava faltando entre diretor_id e telefone
 
@@ -37,7 +37,7 @@ def adicionar_conta_login(id: str, senha: str):
     print("Login adicionado com sucesso.")
 
 def adicionar_aluno(nome: str, idade: int, data_nascimento: str, genero: str, cpf: str, campus: str, curso: str, turma: str, horario_aula: str, senha: str="123"):
-    id = verificacao.criar_id()
+    id = verificacao.criar_id("aluno")
     cursor.execute("""
                 INSERT INTO alunos VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 """, (id, nome, idade, genero, data_nascimento, cpf, campus, curso, turma, horario_aula)
@@ -46,6 +46,57 @@ def adicionar_aluno(nome: str, idade: int, data_nascimento: str, genero: str, cp
     adicionar_conta_login(id, senha)
     conexao.commit()
     print(f"Aluno {nome} adicionado com sucesso com ID {id}")
+    
+def adicionar_professor(nome :str, idade :int, genero :str, data_nascimento: str, cpf: str, materia_ensino: str, senha :str="123"):
+    id = verificacao.criar_id("professor")
+    cursor.execute("""
+                INSERT INTO professores VALUES ( ?, ?, ?, ?, ?, ?, ?)
+                """, (id, nome, idade, genero, data_nascimento, cpf, materia_ensino)
+                )
+                
+    adicionar_conta_login(id, senha)
+    conexao.commit()
+    print(f"Professor {nome} adicionado com sucesso com ID {id}")
+    
+def adicionar_coordenacao(nome: str, idade: int, genero: str, campus: str, data_nascimento: str, cpf: str, cargo: str, senha: str="123"):
+    id = verificacao.criar_id("coordenador")
+    cursor.execute("""
+                INSERT INTO coordenacao VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)
+                """, (id, nome, idade, genero, campus, data_nascimento, cpf, cargo)
+                )
+                
+    adicionar_conta_login(id, senha)
+    conexao.commit()
+    print(f"Coordenador {nome} adicionado com sucesso com ID {id}")
+    
+def adicionar_campus(nome: str, endereco: str, diretor: str, diretor_id: str, telefone: str):
+    cursor.execute("""
+                INSERT INTO campus (nome, endereco, diretor, diretor_id, telefone) VALUES ( ?, ?, ?, ?, ? )
+                """, ( nome, endereco, diretor, diretor_id, telefone)
+                )
+                
+    conexao.commit()
+    print(f"Campus {nome} adicionado com sucesso")
+    
+def adicionar_curso(curso: str, duracao_anos: int, formacao: str):
+    cursor.execute("""
+                INSERT INTO cursos (curso, duracao_anos, formacao) VALUES ( ?, ?, ?)
+                """, (curso, duracao_anos, formacao)
+                )
+                
+    conexao.commit()
+    print(f"Curso {curso} adicionado com sucesso.")
+    
+def adicionar_materia(materia: str, professor: str, professor_id: str, carga_horaria: int):
+    cursor.execute("""
+                INSERT INTO materias (materia, professor, professor_id, carga_horaria) VALUES ( ?, ?, ?, ?)
+                """, (materia, professor, professor_id, carga_horaria)
+                )
+                
+    conexao.commit()
+    print(f"Materia {materia} adicionado com sucesso.")
+        
+
  
 # Mostrar Informações Únicas 
    
@@ -73,3 +124,8 @@ def mostrar_curso(curso: str):
     
 def mostrar_materia(materia: str):
     return mostrar_informacao_unica("materias", "materia", materia)
+
+def mostrar_tudo_tabela(tabela: str):
+    informacao = cursor.execute(f"SELECT * FROM {tabela}").fetchall()
+    return informacao
+
--- main.py
+++ main.py
@@ -4,14 +4,4 @@
 
 import core.database.bancoDeDados as db
 
-# id_aluno, nome, idade, genero, data_nascimento, cpf, campus, curso, turma, horario_aula
-
-
-print(db.mostrar_aluno("teste"))
-print(db.mostrar_professor("0"))
-print(db.mostrar_coordenacao("0"))
-print(db.mostrar_curso("0"))
-print(db.mostrar_campus("0"))
-print(db.mostrar_materia("0"))
-print(db.mostrar_login("0"))
-
+# ids = ["RGNJ0GA", "HVNFVOP", "L63Y6NC"]
